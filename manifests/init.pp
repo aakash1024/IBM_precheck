05a21d17911b353dd41097f00a7ff29b9337b582
akash <aakash.1024@gmail.com>
2016-04-28 00:51:35 +0530

    commit

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,11 @@
-class ibm_precheck {
-  file { "C:\\temp\\bin\\sxs\\": ensure => directory, }
+# class ibm_precheck {
+# file { "C:\\temp\\bin\\sxs\\": ensure => directory, }
 
-  include ibm_precheck::iis_install
-  include ibm_precheck::net_35
-  include ibm_precheck::net_45
-  include ibm_precheck::msmq
-  include ibm_precheck::oadc_install
-  include ibm_precheck::oadc_config
-  # init.pp
-}
+include ibm_precheck::iis_install
+include ibm_precheck::net_35
+include ibm_precheck::net_45
+include ibm_precheck::msmq
+include ibm_precheck::oadc_install
+include ibm_precheck::oadc_config
+# init.pp
+#}
